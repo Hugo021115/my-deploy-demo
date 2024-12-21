a74045eb5647760f432254b1e5f7ec20c0dd7796
Hugo021115 <a1076961102@gmail.com>
2024-12-21 23:26:08 +0800

    初始化项目

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <h1 className="text-4xl font-bold">我的自动部署演示项目</h1>
+        <p className="mt-4 text-xl">这是一个用于演示 GitHub 和 Vercel 自动部署的项目</p>
+      </div>
+    </main>
+  )
+} 
