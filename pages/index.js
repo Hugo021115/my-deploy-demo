0b29bf7f0b90747b50f5fb958042cb46a8e7aafc
Hugo021115 <>
2024-12-22 01:12:22 +0800

    恢复到2.0版本状态

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1 className="text-4xl font-bold">我的自动部署演示项目</h1>
         <p className="mt-4 text-xl">这是一个用于演示 GitHub 和 Vercel 自动部署的项目</p>
+        <p className="mt-4 text-xl text-blue-600">Hugo2.0在做测试</p>
       </div>
     </main>
   )
